e7c01275e26053ebc109575326ba7ffa924d9ca3
Jonah Stein <jonah.s.stein@gmail.com>
2026-06-15 23:04:32 -0400

    setting up main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "kalshi/KalshiAuth.hpp"
 #include "kalshi/KalshiRestClient.hpp"
+#include "kalshi/KalshiWsClient.hpp"
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
 #include <stdexcept>
+#include <queue>
+
 
 std::string readFile(const std::string& path){
     // input file stream
@@ -19,24 +22,32 @@ std::string readFile(const std::string& path){
     return ss.str();
 }
 
-int main(){
-    
-    std::string base_url = std::getenv("KALSHI_API_BASE_URL");
-    const char* api_key_env = std::getenv("KALSHI_API_KEY");
-    if (!api_key_env){
-        throw std::runtime_error("api key not set\n");
+std::string readEnvVar(const std::string& var_name){
+    const char* raw_var = std::getenv(var_name);
+    if (!raw_var){
+        throw std::runtime_error(std::format("{} not set"));
     }
-    std::string api_key = api_key_env;
+    return raw_var;
+}
 
-    const char* pem_path = std::getenv("KALSHI_PEM_PATH");
-    if (!pem_path){
-        throw std::runtime_error("PEM file not found");
-    }
-    std::string pem = readFile(pem_path);
+
+int main(){
+    std::string base_url = readEnvVar("KALSHI_API_BASE_URL");
+    std::string api_key = readEnvVar("KALSHI_API_KEY");
+    std::string pem = readEnvVar("KALSHI_PEM_PATH");
+    std::string ws_url = readEnvVar("KALSHI_WS_URL");
+    std::string ws_connection_path = readEnvVar("KALSHI_WS_CONNECTION_PATH");
+
 
     KalshiAuth auth(pem, api_key);
     KalshiRestClient rest_client(auth, base_url);
-
-
     rest_client.printSeriesInfo("KXHIGHNY");
+    
+
+    KalshiWsClient ws_client(auth, ws_url, ws_connection_path);
+
+    
+
+    
+
 }
